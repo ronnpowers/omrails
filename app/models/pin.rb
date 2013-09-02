bdeefddade4698ed4f2734d124ba649b16951b9f
ronngiang <rgiang1214@gmail.com>
2013-09-02 15:53:40 -0400

    Add Faker Gem and Add Pagination

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,6 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description, :image_remote_url
- 
+  attr_accessible :description, :image, :image_remote_url
+  has_attached_file :image
 
   validates :description, presence: true
   validates :user_id, presence: true 
@@ -9,7 +9,7 @@ class Pin < ActiveRecord::Base
   														 size: { less_than: 5.megabytes }
   
   belongs_to :user
-  has_attached_file :image, styles: { medium: "320x240"}
+  has_attached_file :image, styles: { medium: "320x240>"}
 
   def image_remote_url=(url_value)
   	self.image = URI.parse(url_value) unless url_value.blank?
